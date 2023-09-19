9a775a3e00d9dc15e6b45de9c5053277d85139c3
Andrii Druzhha <113832819+GKAndrey@users.noreply.github.com>
2023-09-19 18:05:29 +0300

    Add app main. Create css. Add app in settings.

--- library/library/urls.py
+++ library/library/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for library project.
+URL configuration for myproject project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -16,7 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from book import views as bookview
+from mains import views as mainview
+from author import views as authview
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('book/', bookview.main),
+    path('author/', authview.main),
+    path('', mainview.main),
 ]
